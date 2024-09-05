62fa4c950a74f22377d9314a566afe79d62981b7
AmourAnurag <anurag875636@gmail.com>
2024-09-05 09:47:43 +0530

    done

--- src/app/components/Upper.js
+++ src/app/components/Upper.js
@@ -54,11 +54,12 @@ const Upper = () => {
           </button>
         </div>
         <div className={styles.content}>
-          Hello! I&rsquo;m Dave, your sales rep here from Salesforce. I&rsquo;ve
-          been working at this awesome company for 3 years now.
-          <br /> <br />I was born and raised in Albany, NY &amp; have been
-          living in Santa Carla for the past 10 years with my wife Tiffany and
-          my 4-year-old twin daughters—Emma and Ella. Both of them are just
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          Hello! I’m Dave, your sales rep here from Salesforce. I’ve been
+          working at this awesome company for 3 years now.
+          <br /> <br />I was born and raised in Albany, NY & have been living in
+          Santa Carla for the past 10 years with my wife Tiffany and my
+          4-year-old twin daughters—Emma and Ella. Both of them are just
           starting school, so my calendar is usually blocked between 9-10 AM.
           This is a...
         </div>
